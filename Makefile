0b7aa27f956901f34d38e620973d1244713284c6
Eric Torti <eric.torti@vivareal.com>
2016-11-25 18:50:18 -0200

    extracting url

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 .PHONY: test
 test:
-	export $$(cat config/local.config) && lein midje
+	export $$(cat config/local.config) && lein midje :autotest
--- test/clocinante/core_test.clj
+++ test/clocinante/core_test.clj
@@ -4,20 +4,28 @@
             [clojure.java.io :as io]
             [clojure.data.json :as json]))
 
-(defn json-from-file
+(defn from-json-file
   [file]
   (do
     (println file)
-    (json/read-str (slurp file))))
+    (json/read-str
+      (slurp file)
+      :key-fn keyword)))
 
-(def mappings
+(def mappings-files
   (let [dir (System/getenv "MAPPINGS_DIR")]
-    (println "fetching mappings")
     (map
-      json-from-file
+      from-json-file
       (filter
         #(not (.isDirectory %))
         (file-seq (io/file dir))))))
+
+(defn extract-url
+  [mapping-file]
+  (:url (:request mapping-file)))
+
+(def mappings
+  (map extract-url mappings-files))
 
 (facts "first test"
        (fact "it tests"
