8029a39f080d0e08cf548853dee406995978c44a
Eric Torti <eric.torti@vivareal.com>
2016-11-25 18:38:24 -0200

    reading file

--- test/clocinante/core_test.clj
+++ test/clocinante/core_test.clj
@@ -1,13 +1,23 @@
 (ns clocinante.core-test
   (:require [midje.sweet :refer :all]
             [clocinante.core :refer :all]
-            [clojure.java.io :refer :all]))
+            [clojure.java.io :as io]
+            [clojure.data.json :as json]))
+
+(defn json-from-file
+  [file]
+  (do
+    (println file)
+    (json/read-str (slurp file))))
 
 (def mappings
   (let [dir (System/getenv "MAPPINGS_DIR")]
     (println "fetching mappings")
-    (println dir)
-    (file-seq (file dir))))
+    (map
+      json-from-file
+      (filter
+        #(not (.isDirectory %))
+        (file-seq (io/file dir))))))
 
 (facts "first test"
        (fact "it tests"
