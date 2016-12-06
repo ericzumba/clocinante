171cdd1e6e38d521b743d3d1bd861cece6c3bf4b
Eric Torti <eric.torti@vivareal.com>
2016-12-06 19:34:07 -0200

    midje plugin

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :profiles {:dev {:dependencies [[midje "1.7.0"]
                                   [org.clojure/data.json "0.2.6"]
                                   [http-kit "2.2.0"]
-                                  [com.cemerick/url "0.1.1"]]}
+                                  [com.cemerick/url "0.1.1"]]
+                   {:plugins [[lein-midje "3.2.1"]]}}
              :midje {}})
